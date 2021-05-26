a3474cb7c87fe55ebb0d3fab666c2ce3ea8a38d1
Jaquiez <gamerdude959@gmail.com>
2021-05-25 20:52:15 -0400

    unfinished

--- DiscordBot/commands/ud.js
+++ DiscordBot/commands/ud.js
@@ -1,25 +1,28 @@
 module.exports = {
     name: 'ud',
     description: 'gets urban dictionary definition of word',
-
+    permissions: [],
     // Unbans a member from their user ID, can't unban from @ mention
     // Also, can't @ mention a member in chat after they have been unbanned because they aren't in the server anymore
     execute(message, args) {
         const fetch = require("node-fetch");
+        const jsdom = require("jsdom");
         const url = "https://www.urbandictionary.com/define.php?term=" + args[0];
         fetch(url).then(function (response) {
             // The API call was successful!
             return response.text();
         }).then(function (html) {
-            // This is the HTML from our response as a text string
-            const jsdom = require("jsdom");
-            const dom = new jsdom.JSDOM(html)
-            dom.serialize();
+            // This is the HTML from our response as a text string    
+            const dom = new jsdom.JSDOM(html);        
+            console.log(html); //Prints all of the content of the html to console
+            var definition = dom.window.document.getElementsByClassName("meaning").item(1).textContent;
+            console.log(definition);// prints this HTMLCollection {}
          
         }).catch(function (err) {
             // There was an error
             console.warn('Something went wrong.', err);
         });
+        
 
     }
 }
